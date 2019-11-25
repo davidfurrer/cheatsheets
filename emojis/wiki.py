1678bd775e7ec5a4a0242b9c919afa24e4f3c75f
fulowa <fulowa@gmail.com>
2019-11-25 23:26:25 +0100

    formated

--- emojis/wiki.py
+++ emojis/wiki.py
@@ -1,15 +1,12 @@
 import wikipedia
 import emoji_data_python
 
-emojis = [
-    emoji.short_name for emoji in emoji_data_python.emoji_data
-]
-#print(emojis)
+emojis = [emoji.short_name for emoji in emoji_data_python.emoji_data]
 
 text = wikipedia.summary("Moon", sentences=50)
 
-d=[x if x.lower() not in emojis else f':{x.lower()}:' for x in text.split(' ')]
-s=' '.join(d)
+d = [x if x.lower() not in emojis else f":{x.lower()}:" for x in text.split(" ")]
+s = " ".join(d)
 
 with open("wiki.md", "w") as f:
     f.write(s)
