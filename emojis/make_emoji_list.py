1e17eef4dd317cf3363ebe234f807f9fbd43c057
fulowa <fulowa@gmail.com>
2019-11-25 22:40:32 +0100

    emoji list

---
+++ emojis/make_emoji_list.py
@@ -0,0 +1,21 @@
+import emoji_data_python
+
+emojis = [
+    (f"`:{emoji.short_name}:`", emoji.char) for emoji in emoji_data_python.emoji_data
+]
+
+header = """
+| Command        | Emoji          | 
+| ------------- |-------------| 
+"""
+
+markdown_all = """"""
+
+for a, b in emojis:
+    markdown_ = f"""
+    | {a}       | {b} | 
+    """
+    markdown_all += markdown_
+
+with open("emoji-list.md", "w") as f:
+    f.write(header + markdown_all)
